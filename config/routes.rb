26c3804385efaa0f2fd3bf4da372efa0759348a2
iPowers <isika.powers@gmail.com>
2021-10-31 19:47:43 -0600

    Add routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'devise/sessions#new'
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  root to: 'home#index'
 
-  devise_for :users
-
-  namespace :devise do
-    get '/', to: 'sessions#new'
-    # get '/users/:id', to: 'sessions#create'
-  end
+  # namespace :devise do
+  #   get '/', to: 'sessions#new'
+  #   # get '/users/:id', to: 'sessions#create'
+  # end
 
   resources :users, only: [:show, :edit, :update]
   # get '/users/:id', to: 'users#show'
